0fb1cb8426ba427a0a4c73303eb63748eb739ff0
taopaokuzi <15387511021@163.com>
2025-04-28 18:48:39 +0800

    4.5&4.6增加日志，增加http测试文件，方便直接在idea测试

--- member/src/main/java/com/taopaokuzi/train/member/config/MemberApplication.java
+++ member/src/main/java/com/taopaokuzi/train/member/config/MemberApplication.java
@@ -1,13 +1,21 @@
 package com.taopaokuzi.train.member.config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ComponentScan("com.taopaokuzi")
 public class MemberApplication {
-    public static void main(String[] args){
-        SpringApplication.run(MemberApplication.class,args);
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 }
