419bfa0cc4c356531902e2ba9b611d421e609862
taopaokuzi <15387511021@163.com>
2025-04-29 20:30:46 +0800

    4.15 实现一个简单的注册接口

--- member/src/main/java/com/taopaokuzi/train/member/controller/MemberController.java
+++ member/src/main/java/com/taopaokuzi/train/member/controller/MemberController.java
@@ -4,6 +4,7 @@ package com.taopaokuzi.train.member.controller;
 import com.taopaokuzi.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,9 @@ public class MemberController {
     public Integer count() {
 
         return memberService.count();
+    }
+    @PostMapping("/register")//进入到这的方法，都会拼上前面的/member
+    public long register(String mobile) {
+        return memberService.register(mobile);
     }
 }
--- member/src/main/java/com/taopaokuzi/train/member/domain/Member.java
+++ member/src/main/java/com/taopaokuzi/train/member/domain/Member.java
@@ -32,4 +32,6 @@ public class Member {
         sb.append("]");
         return sb.toString();
     }
+
+
 }
