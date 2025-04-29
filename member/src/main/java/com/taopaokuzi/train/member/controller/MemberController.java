1664d77ee7122165c24f45febd4c8b1c529693f0
taopaokuzi <15387511021@163.com>
2025-04-29 22:18:23 +0800

    4.19集成校验框架

--- member/src/main/java/com/taopaokuzi/train/member/controller/MemberController.java
+++ member/src/main/java/com/taopaokuzi/train/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.taopaokuzi.train.common.resp.CommonResp;
 import com.taopaokuzi.train.member.req.MemberRegisterReq;
 import com.taopaokuzi.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ public class MemberController {
         return commonResp;
     }
     @PostMapping("/register")//进入到这的方法，都会拼上前面的/member
-    public CommonResp <Long> register(MemberRegisterReq req) {
+    public CommonResp <Long> register(@Valid MemberRegisterReq req) {
         long register = memberService.register(req);
         return new CommonResp<>(register);
     }
