3cc7487acd28693b76c4e7d54d82497327575e55
taopaokuzi <15387511021@163.com>
2025-04-29 22:32:54 +0800

    4.20 使用雪花算法生成数据库ID

---
+++ common/src/main/java/com/taopaokuzi/train/common/util/SnowUtil.java
@@ -0,0 +1,17 @@
+package com.taopaokuzi.train.common.util;
+
+import cn.hutool.core.util.IdUtil;
+
+public class SnowUtil {
+    private static long dataCenterId = 1;  //数据中心
+    private static long workerId = 1;     //机器标识
+
+    public static long getSnowflakeNextId() {
+        return IdUtil.getSnowflake(workerId, dataCenterId).nextId();
+    }
+
+    public static String getSnowflakeNextIdStr() {
+        return IdUtil.getSnowflake(workerId, dataCenterId).nextIdStr();
+    }
+}
+
--- member/src/main/java/com/taopaokuzi/train/member/service/MemberService.java
+++ member/src/main/java/com/taopaokuzi/train/member/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.taopaokuzi.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.IdUtil;
 import com.taopaokuzi.train.common.exception.BusinessException;
 import com.taopaokuzi.train.common.exception.BusinessExceptionEnum;
+import com.taopaokuzi.train.common.util.SnowUtil;
 import com.taopaokuzi.train.member.domain.Member;
 import com.taopaokuzi.train.member.domain.MemberExample;
 import com.taopaokuzi.train.member.mapper.MemberMapper;
@@ -32,7 +34,7 @@ public class MemberService {
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
         Member member = new Member();
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
