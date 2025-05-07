bbc2b211ab39957c6dbfa367186f4184639d02b3
taopaokuzi <15387511021@163.com>
2025-05-07 13:40:22 +0800

    5.8增加短信验证码登录接口，升级hutool，支持列表复制

--- common/src/main/java/com/taopaokuzi/train/common/exception/BusinessExceptionEnum.java
+++ common/src/main/java/com/taopaokuzi/train/common/exception/BusinessExceptionEnum.java
@@ -1,7 +1,9 @@
 package com.taopaokuzi.train.common.exception;
 
 public enum BusinessExceptionEnum {
-    MEMBER_MOBILE_EXIST("手机号已注册");
+    MEMBER_MOBILE_EXIST("手机号已注册"),
+    MEMBER_MOBILE_NOT_EXIST("手请先获取短信验证码"),
+    MEMBER_MOBILE_CODE_ERROR("短信验证码错误")；
     private String desc;
 
     BusinessExceptionEnum(String desc) {
--- member/src/main/java/com/taopaokuzi/train/member/controller/MemberController.java
+++ member/src/main/java/com/taopaokuzi/train/member/controller/MemberController.java
@@ -2,8 +2,10 @@ package com.taopaokuzi.train.member.controller;
 
 
 import com.taopaokuzi.train.common.resp.CommonResp;
+import com.taopaokuzi.train.member.req.MemberLoginReq;
 import com.taopaokuzi.train.member.req.MemberRegisterReq;
 import com.taopaokuzi.train.member.req.MemberSendCodeReq;
+import com.taopaokuzi.train.member.resp.MemberLoginResp;
 import com.taopaokuzi.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
@@ -33,5 +35,10 @@ public class MemberController {
     public CommonResp <Long> sendCode(@Valid MemberSendCodeReq req) {
         memberService.sendCode(req);
         return new CommonResp<>();
+    }
+    @PostMapping("/login")//进入到这的方法，都会拼上前面的/member
+    public CommonResp <MemberLoginResp> login(@Valid MemberLoginReq req) {
+        MemberLoginResp resp = memberService.login(req);
+        return new CommonResp<MemberLoginResp>(resp);
     }
 }
