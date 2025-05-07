a5a3ea0deacac8a07f0449a0b3ba6fb9afcfd8a1
taopaokuzi <15387511021@163.com>
2025-05-07 14:06:51 +0800

    5.9修改gateway解决跨域问题，将后端接口改成body json方式传递参数，前后端链条成功

--- common/src/main/java/com/taopaokuzi/train/common/exception/BusinessExceptionEnum.java
+++ common/src/main/java/com/taopaokuzi/train/common/exception/BusinessExceptionEnum.java
@@ -3,7 +3,7 @@ package com.taopaokuzi.train.common.exception;
 public enum BusinessExceptionEnum {
     MEMBER_MOBILE_EXIST("手机号已注册"),
     MEMBER_MOBILE_NOT_EXIST("手请先获取短信验证码"),
-    MEMBER_MOBILE_CODE_ERROR("短信验证码错误")；
+    MEMBER_MOBILE_CODE_ERROR("短信验证码错误");
     private String desc;
 
     BusinessExceptionEnum(String desc) {
--- member/src/main/java/com/taopaokuzi/train/member/controller/MemberController.java
+++ member/src/main/java/com/taopaokuzi/train/member/controller/MemberController.java
@@ -9,10 +9,7 @@ import com.taopaokuzi.train.member.resp.MemberLoginResp;
 import com.taopaokuzi.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -32,7 +29,7 @@ public class MemberController {
         return new CommonResp<>(register);
     }
     @PostMapping("/send-code")//进入到这的方法，都会拼上前面的/member
-    public CommonResp <Long> sendCode(@Valid MemberSendCodeReq req) {
+    public CommonResp <Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
         memberService.sendCode(req);
         return new CommonResp<>();
     }
