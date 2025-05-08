e42cbfc4f44f38822a209b5ea3f90123c6357ea4
taopaokuzi <15387511021@163.com>
2025-05-09 00:44:09 +0800

    6.7演示过滤器的使用

--- gateway/src/main/java/com/taopaokuzi/train/gateway/config/GatewayApplication.java
+++ gateway/src/main/java/com/taopaokuzi/train/gateway/config/GatewayApplication.java
@@ -16,6 +16,6 @@ public class GatewayApplication {
         SpringApplication app = new SpringApplication(GatewayApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
-        LOG.info("网关地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("网关地址: http://127.0.0.1:{}", env.getProperty("server.port"));
     }
 }
---
+++ gateway/src/main/java/com/taopaokuzi/train/gateway/config/Test1Filter.java
@@ -0,0 +1,23 @@
+package com.taopaokuzi.train.gateway.config;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+
+@Component
+public class Test1Filter implements GlobalFilter, Ordered {
+    private static final Logger LOG =  LoggerFactory.getLogger(Test1Filter.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        LOG.info("Test1Filter");
+        return chain.filter(exchange);
+    }
+    @Override
+    public int getOrder(){
+        return 1;
+    }
+}
