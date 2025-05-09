900b0c92262d5ac60f843663eff47e3b633a9753
taopaokuzi <15387511021@163.com>
2025-05-09 22:48:52 +0800

    7.8增加乘车人列表查询接口，只能查看当前全员自己添加的乘客

--- member/src/main/java/com/taopaokuzi/train/member/controller/PassengerController.java
+++ member/src/main/java/com/taopaokuzi/train/member/controller/PassengerController.java
@@ -1,13 +1,19 @@
 package com.taopaokuzi.train.member.controller;
 
 
+import com.taopaokuzi.train.common.context.LoginMemberContext;
 import com.taopaokuzi.train.common.resp.CommonResp;
+import com.taopaokuzi.train.member.domain.Passenger;
+import com.taopaokuzi.train.member.req.PassengerQueryReq;
 import com.taopaokuzi.train.member.req.PassengerSaveReq;
+import com.taopaokuzi.train.member.resp.PassengerQueryResp;
 import com.taopaokuzi.train.member.service.MemberService;
 import com.taopaokuzi.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -16,7 +22,13 @@ public class PassengerController {
     private PassengerService passengerService;
     @PostMapping("/save")//进入到这的方法，都会拼上前面的/member
     public CommonResp <Object> register(@Valid @RequestBody PassengerSaveReq req) {
-        passengerService.save(req);
+        passengerService.save(req);//保存不需要返回值
         return new CommonResp<>();
+    }
+    @GetMapping("/query-list")//进入到这的方法，都会拼上前面的/member
+    public CommonResp <List<PassengerQueryResp>> queryList(@Valid  PassengerQueryReq req) {
+        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> list= passengerService.queryList(req);//查询需要返回值
+        return new CommonResp<>(list);
     }
 }
--- member/src/main/java/com/taopaokuzi/train/member/service/PassengerService.java
+++ member/src/main/java/com/taopaokuzi/train/member/service/PassengerService.java
@@ -1,13 +1,21 @@
 package com.taopaokuzi.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.taopaokuzi.train.common.context.LoginMemberContext;
 import com.taopaokuzi.train.common.util.SnowUtil;
 import com.taopaokuzi.train.member.domain.Passenger;
+import com.taopaokuzi.train.member.domain.PassengerExample;
 import com.taopaokuzi.train.member.mapper.PassengerMapper;
+import com.taopaokuzi.train.member.req.PassengerQueryReq;
 import com.taopaokuzi.train.member.req.PassengerSaveReq;
+import com.taopaokuzi.train.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class PassengerService {
     @Resource
@@ -20,5 +28,14 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+    public List<PassengerQueryResp> queryList(PassengerQueryReq req){
+        PassengerExample passengerExample= new PassengerExample();
+        PassengerExample.Criteria criteria=passengerExample.createCriteria();//要把它放到外部，如果多个if的话，那么会以最后一个if里的create为准
+        if(ObjectUtil.isNotNull(req.getMemberId())){
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
+        List<Passenger> passengerList= passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(passengerList,PassengerQueryResp.class);
     }
 }
